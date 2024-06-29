955dbd7436636c11efa20623211773b4f7851ef6
Benjamin <153400127+Santiago235v902@users.noreply.github.com>
2024-06-29 01:44:26 -0300

    Add files via upload

---
+++ 01-operador.js
@@ -0,0 +1,53 @@
+let a = 2;
+let b = 5;
+//declaracion
+
+
+
+// console.log(a+b,'suma'); //adicion
+// console.log(a-b);//resta
+// console.log(a*b);//multiplicar
+// console.log(a/b);//dividision
+// console.log(a%b);//modulo
+// console.log(a+b)//potencia
+// console.log(a**b)//poterncia
+
+
+//incrementar
+console.log(a++)
+console.log(++a);
+console.log(a);
+//decrementar
+console.log(--a);
+console.log(a--);
+console.log(a);
+
+
+
+let c = 5;
+c= c+5;
+  
+c += 5;
+console.log(c)
+
+
+let d = 10
+console.log(d>5);
+console.log(d>=5)
+console.log(d<5)
+console.log(d<=5)
+console.log(d == 10);
+console.log(d != 10);
+console.log(d == "10");
+console.log(d === "10");
+console.log(true && true);
+console.log(false && false);
+console.log(false && true);
+console.log(false && false);
+
+
+
+let entra = true;
+let sus = true;
+
+console.log(entra && sus);
